eb9a0529f26e1b83e32db394ad230b7ce1f13d00
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-01-29 15:51:33 +0530

    DB and Application settings

---
+++ src/main/java/com/mayu/book/BookNetworkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mayu.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkBackendApplication.class, args);
+	}
+
+}
