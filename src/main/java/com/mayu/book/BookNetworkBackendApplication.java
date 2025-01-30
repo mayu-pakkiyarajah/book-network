9efc83a9fb3e6f77c0841ec3f392b6fbee079682
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-01-30 22:53:58 +0530

    User

--- src/main/java/com/mayu/book/BookNetworkBackendApplication.java
+++ src/main/java/com/mayu/book/BookNetworkBackendApplication.java
@@ -2,8 +2,10 @@ package com.mayu.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookNetworkBackendApplication {
 
 	public static void main(String[] args) {
