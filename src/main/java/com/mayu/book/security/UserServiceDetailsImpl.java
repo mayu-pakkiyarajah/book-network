ab5482932e4b9ecc6f9e266b51db359e47428f53
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-01-31 00:20:30 +0530

    User Service Details Update

--- src/main/java/com/mayu/book/security/UserServiceDetailsImpl.java
+++ src/main/java/com/mayu/book/security/UserServiceDetailsImpl.java
@@ -1,12 +1,24 @@
 package com.mayu.book.security;
 
+import com.mayu.book.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+
+@Service
+@RequiredArgsConstructor
 public class UserServiceDetailsImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("Unable to find user"));
     }
 }
