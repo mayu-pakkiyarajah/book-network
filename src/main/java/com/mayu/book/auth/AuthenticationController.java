2c00f8f664800df628f7066048c9ad959c9426b1
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-01 16:15:35 +0530

    Authentication and Register

---
+++ src/main/java/com/mayu/book/auth/AuthenticationController.java
@@ -0,0 +1,26 @@
+package com.mayu.book.auth;
+
+import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+@Table(name = "Authentication")
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<?> register(
+            @RequestBody @Valid RegistrationRequest request) {
+        service.register(request);
+        return ResponseEntity.accepted().build();
+    }
+
+}
---
+++ src/main/java/com/mayu/book/auth/AuthenticationService.java
@@ -0,0 +1,11 @@
+package com.mayu.book.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    public void register(RegistrationRequest request) {
+
+
+    }
+}
