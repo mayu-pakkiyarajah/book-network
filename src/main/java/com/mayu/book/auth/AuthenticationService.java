d51ff7c1c3ac28c68d0a5130795105aaf8a6256b
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-01 22:50:55 +0530

    AuthService with email code sending

--- src/main/java/com/mayu/book/auth/AuthenticationService.java
+++ src/main/java/com/mayu/book/auth/AuthenticationService.java
@@ -1,11 +1,82 @@
 package com.mayu.book.auth;
 
+import com.mayu.book.role.RoleRepository;
+import com.mayu.book.user.Token;
+import com.mayu.book.user.TokenRepository;
+import com.mayu.book.user.User;
+import com.mayu.book.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+
     public void register(RegistrationRequest request) {
 
+        var userRole = roleRepository.findByName("USER")
+                .orElseThrow(() -> new IllegalStateException("User not found"));
+
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+
+        userRepository.save(user);
+
+        sendValidationEmail(user);
+    }
+
+    private void sendValidationEmail(User user) {
+        
+        var newToken = generateAndSaveActivationToken(user);
+
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+
+        //Email verification code generation
+        String generatedToken = activationToken(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+
+        tokenRepository.save(token);
+
+        return generatedToken;
+    }
+
+    private String activationToken(int length) {
+
+        String characters = "0123456789";
+        StringBuilder tokenBuilder = new StringBuilder(length);
+        SecureRandom secureRandom = new SecureRandom();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length());
+            tokenBuilder.append(characters.charAt(randomIndex));
+        }
+
+        return tokenBuilder.toString();
 
     }
 }
