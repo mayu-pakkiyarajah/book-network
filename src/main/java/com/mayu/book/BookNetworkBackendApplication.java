5c8d6ff9642b02b5ed65d1aed451b0d63b9d3ab2
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-03 11:23:58 +0530

    Update BNApplicationBEnd

--- src/main/java/com/mayu/book/BookNetworkBackendApplication.java
+++ src/main/java/com/mayu/book/BookNetworkBackendApplication.java
@@ -1,7 +1,11 @@
 package com.mayu.book;
 
+import com.mayu.book.role.Role;
+import com.mayu.book.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -12,6 +16,17 @@ public class BookNetworkBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookNetworkBackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isPresent()) {
+				roleRepository.save(
+						Role.builder().name("USER").build()
+				);
+			}
+		};
 	}
 
 }
