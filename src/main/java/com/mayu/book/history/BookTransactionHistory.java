cc376ea7ce9ad1878d9bec07b9e2d627987e2805
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-07 23:06:53 +0530

    Book transaction history created

---
+++ src/main/java/com/mayu/book/history/BookTransactionHistory.java
@@ -0,0 +1,22 @@
+package com.mayu.book.history;
+
+import com.mayu.book.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+    private boolean returned;
+    private boolean returnApproved;
+}
