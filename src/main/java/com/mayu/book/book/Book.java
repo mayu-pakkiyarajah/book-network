d2f12f685989b7023a46759f06d6dfa5f85fff65
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-07 22:32:20 +0530

    Book entity creation

---
+++ src/main/java/com/mayu/book/book/Book.java
@@ -0,0 +1,47 @@
+package com.mayu.book.book;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String title;
+
+    private String authorName;
+
+    private String isbn;
+
+    private String synopsis;
+
+    private String bookCover;
+
+    private boolean archived;
+
+    private boolean shareable;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDate createdDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDate lastModifiedDate;
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private Integer createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
+}
