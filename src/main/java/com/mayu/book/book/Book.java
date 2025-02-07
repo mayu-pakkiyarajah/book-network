25647dcaa10b87a7de207ff657573c1b567d6410
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-07 23:20:12 +0530

    Book entity update

--- src/main/java/com/mayu/book/book/Book.java
+++ src/main/java/com/mayu/book/book/Book.java
@@ -1,12 +1,17 @@
 package com.mayu.book.book;
 
 import com.mayu.book.common.BaseEntity;
+import com.mayu.book.feedback.Feedback;
+import com.mayu.book.history.BookTransactionHistory;
+import com.mayu.book.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 
 @Getter
@@ -24,4 +29,14 @@ public class Book extends BaseEntity {
     private String bookCover;
     private boolean archived;
     private boolean shareable;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "book")
+    private List<Feedback> feedbacks;
+
+    @OneToMany(mappedBy = "book")
+    private List<BookTransactionHistory> histories;
 }
