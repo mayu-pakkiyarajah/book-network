4bc0b8014ad8b9a43692b52b11cf7c67885edd6c
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-07 22:38:47 +0530

    Feedback entity creation

---
+++ src/main/java/feedback/Feedback.java
@@ -0,0 +1,39 @@
+package feedback;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Feedback {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private Double note;
+
+    private String comment;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDate createdDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDate lastModifiedDate;
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private Integer createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
+}
