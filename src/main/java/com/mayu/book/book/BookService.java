630d5f88c52bb492e1339151422ae8d31102f6ba
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-09 22:31:12 +0530

    Book service update

--- src/main/java/com/mayu/book/book/BookService.java
+++ src/main/java/com/mayu/book/book/BookService.java
@@ -20,4 +20,8 @@ public class BookService {
         book.setOwner(user);
         return bookRepository.save(book).getId();
     }
+
+    public BookResponse findById(Integer bookId) {
+        return null;
+    }
 }
