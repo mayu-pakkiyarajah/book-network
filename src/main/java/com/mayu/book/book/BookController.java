c2a45bead0092852a8e24f1a202d3e6006fc37a8
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-09 23:31:50 +0530

    Book controller with find all books

--- src/main/java/com/mayu/book/book/BookController.java
+++ src/main/java/com/mayu/book/book/BookController.java
@@ -29,4 +29,13 @@ public class BookController {
     ){
         return ResponseEntity.ok(service.findById(bookId));
     }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.findAllBooks(page, size, connectedUser));
+    }
 }
