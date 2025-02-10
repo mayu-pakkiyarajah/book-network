c71ee5a80834667cb52d6dcc00d6f7aaf61aaa24
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-11 00:06:34 +0530

    find owner by id updated

--- src/main/java/com/mayu/book/book/BookService.java
+++ src/main/java/com/mayu/book/book/BookService.java
@@ -57,7 +57,19 @@ public class BookService {
     public PageResponse<BookResponse> findAllBooksByOwner(int page, int size, Authentication connectedUser) {
         User user = ((User) connectedUser.getPrincipal());
         Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
-        Page<Book> books = bookRepository.findAll(spec, pageable);
-        return null;
+        Page<Book> books = bookRepository.findAll(BookSpecification.withOwnerId(user.getId()), pageable);
+        List<BookResponse> bookResponse = books.stream()
+                .map(bookMapper::toBookResponse)
+                .toList();
+
+        return new PageResponse<>(
+                bookResponse,
+                books.getNumber(),
+                books.getSize(),
+                books.getTotalElements(),
+                books.getTotalPages(),
+                books.isFirst(),
+                books.isLast()
+        );
     }
 }
