8296078653d2583c6c0d9911689575d7927520e6
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-11 00:07:26 +0530

    correct all typos in book specification

--- src/main/java/com/mayu/book/book/BookSpecification.java
+++ src/main/java/com/mayu/book/book/BookSpecification.java
@@ -1,8 +1,11 @@
 package com.mayu.book.book;
 
+import org.springframework.data.jpa.domain.Specification;
+
 public class BookSpecification {
 
-    public static Specification<Book> with OwnerId(Integer ownerId) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("owner").get("id"), ownerId)
+    public static Specification<Book> withOwnerId(Integer ownerId) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("owner").get("id"), ownerId);
     }
 }
