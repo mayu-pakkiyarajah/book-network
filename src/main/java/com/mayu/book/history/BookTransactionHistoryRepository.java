30e36692b2de8202a27162734f909a867f23fdf0
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-11 00:32:02 +0530

    Book transaction history repository interface creation

---
+++ src/main/java/com/mayu/book/history/BookTransactionHistoryRepository.java
@@ -0,0 +1,17 @@
+package com.mayu.book.history;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+
+public interface BookTransactionHistoryRepository extends JpaRepository<BookTransactionHistory, Integer> {
+    @Query("""
+            SELECT history
+            FROM BookTransactionHistory history
+            WHERE history.user.id = :userId
+""")
+    Page<BookTransactionHistory> finAllBorrowedBooks(Pageable pageable, Integer userId);
+}
