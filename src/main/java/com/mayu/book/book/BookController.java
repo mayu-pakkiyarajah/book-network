9f3726504d0c63d2589a7e419c941719f4952de7
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-12 06:53:37 +0530

    Book controller updated with returned books

--- src/main/java/com/mayu/book/book/BookController.java
+++ src/main/java/com/mayu/book/book/BookController.java
@@ -50,12 +50,22 @@ public class BookController {
     }
 
     @GetMapping("/borrowed")
-    public ResponseEntity<PageResponse<BookResponse>> findAllBorrowedBooks(
+    public ResponseEntity<PageResponse<BorrowedBookResponse>> findAllBorrowedBooks(
             @RequestParam(name = "page", defaultValue = "0", required = false) int page,
             @RequestParam(name = "size", defaultValue = "10", required = false) int size,
             Authentication connectedUser
     )
     {
         return ResponseEntity.ok(service.findAllBorrowedBooks(page, size, connectedUser));
+    }
+
+    @GetMapping("/returned")
+    public ResponseEntity<PageResponse<BorrowedBookResponse>> findAllReturnedBooks(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    )
+    {
+        return ResponseEntity.ok(service.findAllReturnedBooks(page, size, connectedUser));
     }
 }
