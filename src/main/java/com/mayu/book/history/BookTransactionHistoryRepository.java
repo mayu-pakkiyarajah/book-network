fbf35c7972efe78a7f7d51b3ba7b403edd559539
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-15 22:48:50 +0530

    Return borrowed book query creation in book controller

--- src/main/java/com/mayu/book/history/BookTransactionHistoryRepository.java
+++ src/main/java/com/mayu/book/history/BookTransactionHistoryRepository.java
@@ -31,4 +31,14 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
             AND bookTransactionHistory.returnApproved = false
 """)
     boolean isAlreadyBorrowedByUser(Integer bookId, Integer userId);
+
+    @Query("""
+            SELECT transaction
+            FROM BookTransactionHistory transaction
+            WHERE transaction.user.id = :userId
+            AND transaction.book.id = :bookId
+            AND transaction.returned = false
+            AND transaction.returnApproved = false
+""")
+    BookTransactionHistory findByBookIdAndUserId(Integer bookId, Integer userId);
 }
