ea10f8befdc3341c5b501839b575b57745d133ee
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-15 22:45:22 +0530

    Return borrowed book option creation in book controller

--- src/main/java/com/mayu/book/book/BookController.java
+++ src/main/java/com/mayu/book/book/BookController.java
@@ -92,4 +92,12 @@ public class BookController {
     ){
         return ResponseEntity.ok(service.borrowBook(bookId, connectedUser));
     }
+
+    @PatchMapping("/borrow/return/{book-id}")
+    public ResponseEntity<Integer> returnBorrowBook(
+            @PathVariable("book-id") Integer bookId,
+            Authentication connectedUser
+    ){
+        return ResponseEntity.ok(service.returnBorrowBook(bookId, connectedUser));
+    }
 }
