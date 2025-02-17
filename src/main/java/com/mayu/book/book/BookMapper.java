4416b5c876efaeee1426c5f918cab4f6970948ef
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-17 23:40:19 +0530

    Read file from location

--- src/main/java/com/mayu/book/book/BookMapper.java
+++ src/main/java/com/mayu/book/book/BookMapper.java
@@ -1,5 +1,6 @@
 package com.mayu.book.book;
 
+import com.mayu.book.file.FileUtils;
 import com.mayu.book.history.BookTransactionHistory;
 import org.springframework.stereotype.Service;
 
@@ -28,7 +29,7 @@ public class BookMapper {
                 .archived(book.isArchived())
                 .shareable(book.isShareable())
                 .owner(book.getOwner().getName())
-                //.cover()
+                .cover(FileUtils.readFileFromLocation(book.getBookCover()))
                 .build();
     }
 
